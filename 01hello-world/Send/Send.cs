f0d15ced5a1b1f692478858c7d42cf92e51c01f7
Ben Fleuty <bfleuty@example.com>
2022-02-24 00:43:56 +0000

    Modernise and add comments

--- 01hello-world/Send/Send.cs
+++ 01hello-world/Send/Send.cs
@@ -6,18 +6,24 @@ class Send
 {
     public static void Main()
     {
+        // Create the connection factory
         var factory = new ConnectionFactory() { HostName = "localhost" };
-        using(var connection = factory.CreateConnection())
-        using(var channel = connection.CreateModel())
-        {
-            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        using var connection = factory.CreateConnection();
 
-            string message = "Hello, World!";
-            var body = Encoding.UTF8.GetBytes(message);
+        // Create the channel for sending the message
+        using var channel = connection.CreateModel();
+        // define queue name and properties
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
-            Console.WriteLine(" [x] Sent {0}", message);
-        }
+        // define the message
+        string message = "Hello, World!";
+        // encode the message 
+        var body = Encoding.UTF8.GetBytes(message);
+
+        // publish (send) the message to the queue
+        channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+        Console.WriteLine(" [x] Sent {0}", message);
 
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
