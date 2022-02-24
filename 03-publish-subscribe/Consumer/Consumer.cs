e1184429544e3efb0624e161027e34878b55db8c
Ben Fleuty <bfleuty@example.com>
2022-02-24 18:31:11 +0000

    Import 02 with correct naming

---
+++ 03-publish-subscribe/Consumer/Consumer.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Consumer
+{
+    public static void Main()
+    {
+        // Create the connection factory
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+
+        // Create the channel for receiving the message
+        using var channel = connection.CreateModel();
+        // define the queue name to listen to 
+        channel.QueueDeclare(queue: "task_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        // create a new consumer that has events
+        var consumer = new EventingBasicConsumer(channel);
+        var random = new Random();
+
+        // event: when the consumer has received a message...
+        // this event will trigger on every message until the program exits
+        consumer.Received += (model, ea) =>
+        {
+            // ...received message converted to an array of bytes
+            var body = ea.Body.ToArray();
+            // 'body' is converted from UTF8 encoded bytes into a string
+            var message = Encoding.UTF8.GetString(body);
+            // output the received message to the console
+            Console.WriteLine($" [x] Received {message}");
+
+            int sleepMultiplier = random.Next(0,6);
+            Thread.Sleep(sleepMultiplier * 1000);
+
+            Console.WriteLine($" [x] Completed [request #{ea.DeliveryTag}] in ~{sleepMultiplier}s");
+
+            // Note: it is possible to access the channel via
+            //       ((EventingBasicConsumer)sender).Model here
+            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        };
+
+        // consume and acknowledge the message
+        channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+
+        // when enter is hit, the listener will terminate
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+    }
+}
---
+++ 03-publish-subscribe/LogEmitter/LogEmitter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+
+class LogEmitter
+{
+    public static void Main(string[] args)
+    {
+        // Create the connection factory
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+
+        // Create the channel for sending the message
+        using var channel = connection.CreateModel();
+        // define queue name and properties
+        channel.QueueDeclare(queue: "task_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        // define the message
+        string message = GetMessage(args);
+        // encode the message 
+        var body = Encoding.UTF8.GetBytes(message);
+
+        // create the properties for the task
+        var properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
+        // publish (send) the message to the queue
+        for (var i = 0; i < 10000; i++)
+        {
+            channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+            Thread.Sleep(100);
+        }
+
+        Console.WriteLine($" [x] Sent {message}");
+        Console.WriteLine("Exiting.");
+    }
+
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello, World!");
+    }
+}
