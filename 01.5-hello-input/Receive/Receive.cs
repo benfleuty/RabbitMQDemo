1866d0c8c72520d01eedc79bf4615a974e5f3f76
Ben Fleuty <bfleuty@outlook.com>
2022-02-24 00:49:17 +0000

    Allow the user to input a message that is sent to the queue.
    If the user inputs a null message then "Hello, World!" is used in its place.

---
+++ 01.5-hello-input/Receive/Receive.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+        // Create the connection factory
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+
+        // Create the channel for receiving the message
+        using var channel = connection.CreateModel();
+        // define the queue name to listen to 
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        // create a new consumer that has events
+        var consumer = new EventingBasicConsumer(channel);
+
+        // event: when the consumer has received a message...
+        // this event will trigger on every message until the program exits
+        consumer.Received += (model, ea) =>
+        {
+            // ...received message converted to an array of bytes
+            var body = ea.Body.ToArray();
+            // 'body' is converted from UTF8 encoded bytes into a string
+            var message = Encoding.UTF8.GetString(body);
+            // output the received message to the console
+            Console.WriteLine(" [x] Received {0}", message);
+        };
+
+        // consume and acknowledge the message
+        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+        // when enter is hit, the listener will terminate
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+    }
+}
---
+++ 01.5-hello-input/Send/Send.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        // Create the connection factory
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+
+        // Create the channel for sending the message
+        using var channel = connection.CreateModel();
+        // define queue name and properties
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        // get the message from the user
+        Console.Write($"Enter a message to send to the 'hello' queue: ");
+        string message = Console.ReadLine() ?? "Hello, World!";
+        // obvious error handling needed here, but it is a demo...
+
+
+        // encode the message 
+        var body = Encoding.UTF8.GetBytes(message);
+
+        // publish (send) the message to the queue
+        channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+        Console.WriteLine(" [x] Sent {0}", message);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
